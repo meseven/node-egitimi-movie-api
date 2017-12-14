77c3c9a0b9eb8dd25a3d0d74ba8faea10f59e0f3
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-14 17:46:50 +0300

    movies endpoint post method

--- helper/db.js
+++ helper/db.js
@@ -9,4 +9,6 @@ module.exports = () => {
   mongoose.connection.on('error', (err) => {
     console.log('MongoDB: Error', err);
   });
+
+  mongoose.Promise = global.Promise;
 };
--- routes/movie.js
+++ routes/movie.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({ status: 1 });
+// Models
+const Movie = require('../models/Movie');
+
+router.post('/', (req, res, next) => {
+	// const { title, imdb_score, category, country, year } = req.body;
+
+	const movie = new Movie(req.body);
+	const promise = movie.save();
+
+	promise.then((data) => {
+		res.json({ status: 1 });
+	}).catch((err) => {
+		res.json(err);
+	});
 });
 
 module.exports = router;
