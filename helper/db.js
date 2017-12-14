05bfd41594efa5f656ca771217cbf469309f71fd
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-14 14:23:48 +0300

    lint

--- helper/db.js
+++ helper/db.js
@@ -4,7 +4,7 @@ module.exports = () => {
   mongoose.connect('mongodb://movie_user:abcd1234@ds139436.mlab.com:39436/movie-api', { useMongoClient: true});
 
   mongoose.connection.on('open', () => {
-      console.log('MongoDB: Connected');
+    console.log('MongoDB: Connected');
   });
   mongoose.connection.on('error', (err) => {
     console.log('MongoDB: Error', err);
