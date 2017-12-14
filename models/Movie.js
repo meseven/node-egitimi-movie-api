307a2189c015082509bf9c82e8a029a94822043c
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-14 22:34:18 +0300

    top10 endpoint

--- models/Movie.js
+++ models/Movie.js
@@ -11,7 +11,7 @@ const MovieSchema = new Schema({
     country: String,
     year: Number,
     imdb_score: Number,
-    date: {
+    createdAt: {
         type: Date,
         default: Date.now
     }
--- routes/movie.js
+++ routes/movie.js
@@ -14,6 +14,17 @@ router.get('/', (req, res) => {
 	})
 });
 
+// Top 10 list
+router.get('/top10', (req, res) => {
+	const promise = Movie.find({ }).limit(10).sort({ imdb_score: -1 });
+
+	promise.then((data) => {
+		res.json(data);
+	}).catch((err) => {
+		res.json(err);
+	})
+});
+
 router.get('/:movie_id', (req, res, next) => {
 	const promise = Movie.findById(req.params.movie_id);
 
@@ -27,6 +38,38 @@ router.get('/:movie_id', (req, res, next) => {
 	});
 });
 
+router.put('/:movie_id', (req, res, next) => {
+	const promise = Movie.findByIdAndUpdate(
+		req.params.movie_id,
+		req.body,
+		{
+			new: true
+		}
+	);
+
+	promise.then((movie) => {
+		if (!movie)
+			next({ message: 'The movie was not found.', code: 99 });
+
+		res.json(movie);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
+router.delete('/:movie_id', (req, res, next) => {
+	const promise = Movie.findByIdAndRemove(req.params.movie_id);
+
+	promise.then((movie) => {
+		if (!movie)
+			next({ message: 'The movie was not found.', code: 99 });
+
+		res.json({ status: 1 });
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
 router.post('/', (req, res, next) => {
 	// const { title, imdb_score, category, country, year } = req.body;
 
@@ -34,7 +77,7 @@ router.post('/', (req, res, next) => {
 	const promise = movie.save();
 
 	promise.then((data) => {
-		res.json({ status: 1 });
+		res.json(data);
 	}).catch((err) => {
 		res.json(err);
 	});
