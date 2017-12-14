856eadae6b2c7baa2d9677e765ecb52c31dfc414
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-14 22:42:22 +0300

    /movies/between endpoint

--- routes/movie.js
+++ routes/movie.js
@@ -83,4 +83,20 @@ router.post('/', (req, res, next) => {
 	});
 });
 
+// Between
+router.get('/between/:start_year/:end_year', (req, res) => {
+	const { start_year, end_year } = req.params;
+	const promise = Movie.find(
+		{
+			year: { "$gte": parseInt(start_year), "$lte": parseInt(end_year) }
+		}
+	);
+
+	promise.then((data) => {
+		res.json(data);
+	}).catch((err) => {
+		res.json(err);
+	})
+});
+
 module.exports = router;
