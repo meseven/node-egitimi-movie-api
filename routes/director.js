0e6492cc251496e7604bc3fe837a1e99fa3c0e56
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-15 17:09:10 +0300

    /api/directors/:director_id [DELETE]

--- routes/director.js
+++ routes/director.js
@@ -113,4 +113,36 @@ router.get('/:director_id', (req, res) => {
 	});
 });
 
+router.put('/:director_id', (req, res, next) => {
+	const promise = Director.findByIdAndUpdate(
+		req.params.director_id,
+		req.body,
+		{
+			new: true
+		}
+	);
+
+	promise.then((director) => {
+		if (!director)
+			next({ message: 'The director was not found.', code: 99 });
+
+		res.json(director);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
+router.delete('/:director_id', (req, res, next) => {
+	const promise = Director.findByIdAndRemove(req.params.director_id);
+
+	promise.then((director) => {
+		if (!director)
+			next({ message: 'The director was not found.', code: 99 });
+
+		res.json({ status: 1 });
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
 module.exports = router;
