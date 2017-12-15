acefdfe673fd9bee7f2cd3c9ceca0d572c92d432
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-15 20:42:58 +0300

    movie validations

--- models/Movie.js
+++ models/Movie.js
@@ -4,13 +4,31 @@ const Schema  = mongoose.Schema;
 const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
-        type: String,
-        required: true
+			type: String,
+			required:  [true, '`{PATH}` alanı zorunludur.'],
+			maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır '],
+			minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+    	type: String,
+			maxlength: 30,
+			minlength: 1,
+		},
+    country: {
+			type: String,
+			maxlength: 30,
+			minlength: 1,
+		},
+    year: {
+    	type: Number,
+			max: 2040,
+			min: 1900
+		},
+    imdb_score: {
+    	type: Number,
+			max: 10,
+			min: 0
+		},
     createdAt: {
         type: Date,
         default: Date.now
