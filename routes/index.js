90c28a1127dbdc3e8f9b21627c4df695893dcead
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-16 12:06:00 +0300

    bcrypt

--- routes/index.js
+++ routes/index.js
@@ -13,18 +13,20 @@ router.get('/', (req, res, next) => {
 
 router.post('/register', (req, res, next) => {
 	const { username, password } = req.body;
-	const user = new User({
-		username,
-		password
+
+	bcrypt.hash(password, 10).then((hash) => {
+		const user = new User({
+			username,
+			password: hash
+		});
+
+		const promise = user.save();
+		promise.then((data) => {
+			res.json(data)
+		}).catch((err) => {
+			res.json(err);
+		})
 	});
-
-	const promise = user.save();
-	promise.then((data) => {
-		res.json(data)
-	}).catch((err) => {
-		res.json(err);
-	})
-
 });
 
 module.exports = router;
