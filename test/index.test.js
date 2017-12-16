e42174958fba5a227594b53c16dac80a19dad4c9
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-16 21:02:09 +0300

    test stack added

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+describe('Node Server', () => {
+	it('(GET /) anasayfayı döndürür', (done) => {
+		chai.request(server)
+			.get('/')
+			.end((err, res) => {
+				res.should.have.status(200);
+				done();
+			})
+	});
+});
+
