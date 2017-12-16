c03df83c050b798bf3bf84b7c327cab82ccdfc8f
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-16 21:48:32 +0300

    mocha test script

--- test/api/index.test.js
+++ test/api/index.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
-const server = require('../app');
+const server = require('../../app');
 
 chai.use(chaiHttp);
 
