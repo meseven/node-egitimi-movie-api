89381dcd52402e3e22e6c684f4e2cb64600629e7
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-17 12:53:22 +0300

    /GET/:director_id movie tests

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -4,7 +4,7 @@ const should = chai.should();
 const server = require('../../app');
 chai.use(chaiHttp);
 
-let token;
+let token, movieId;
 
 describe('/api/movies tests', () => {
 	before((done) => {
@@ -29,7 +29,6 @@ describe('/api/movies tests', () => {
 				});
 		})
 	});
-
 	describe('/POST movie', () => {
 		it('it should POST a movie', (done) => {
 			const movie = {
@@ -54,6 +53,26 @@ describe('/api/movies tests', () => {
 					res.body.should.have.property('country');
 					res.body.should.have.property('year');
 					res.body.should.have.property('imdb_score');
+					movieId = res.body._id;
+					done();
+				});
+		});
+	});
+	describe('/GET/:director_id movie', () => {
+		it('it should GET a movie by the given id', (done) => {
+			chai.request(server)
+				.get('/api/movies/'+ movieId)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('title');
+					res.body.should.have.property('director_id');
+					res.body.should.have.property('category');
+					res.body.should.have.property('country');
+					res.body.should.have.property('year');
+					res.body.should.have.property('imdb_score');
+					res.body.should.have.property('_id').eql(movieId);
 					done();
 				});
 		});
