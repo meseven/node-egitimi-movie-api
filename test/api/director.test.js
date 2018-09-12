f9d38f163a3d9abf1a101b3e53913f79b49103cc
Mehmet Seven <mehmetseven0@gmail.com>
2018-09-12 19:59:57 +0300

    movie tests:lint

--- test/api/director.test.js
+++ test/api/director.test.js
@@ -9,26 +9,31 @@ let token, directorId;
 
 describe('Director tests', () => {
 	before('Get Token', (done) => {
-		chai.request(server).post('/authenticate').send({username: 'mseven', password: '12345'}).end((err, res) => {
-			if (err) {
-				throw err;
-			}
-			token = res.body.token;
-			done();
-		});
+		chai.request(server)
+			.post('/authenticate')
+			.send({username: 'mseven', password: '12345'})
+			.end((err, res) => {
+				if (err)
+					throw err;
+
+				token = res.body.token;
+				done();
+			});
 	});
 
 	describe('/GET Directors', () => {
 		it('Get all directors records', (done) => {
-			chai.request(server).get('/api/directors').set('x-access-token', token).end((err, res) => {
-				if (err) {
-					throw err;
-				}
-				res.should.have.status(200);
-				res.body.should.be.a('array');
-				done();
-			});
+			chai.request(server)
+				.get('/api/directors')
+				.set('x-access-token', token)
+				.end((err, res) => {
+					if (err)
+						throw err;
 
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					done();
+				});
 		});
 	});
 
@@ -39,32 +44,37 @@ describe('Director tests', () => {
 				surname: 'Deep',
 				bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing volutpat.'
 			};
-			chai.request(server).post('/api/directors').send(director).set('x-access-token', token).end((err, res) => {
-				console.log(res.body);
-				if (err) {
-					throw err;
-				}
-				res.should.have.status(200);
-				res.body.should.be.a('object');
-				res.body.should.have.property('name');
-				res.body.should.have.property('surname');
-				res.body.should.have.property('bio');
-				directorId = res.body._id;
-				done();
-			});
+			chai.request(server).post('/api/directors')
+				.send(director)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					if (err)
+						throw err;
+
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('name');
+					res.body.should.have.property('surname');
+					res.body.should.have.property('bio');
+					directorId = res.body._id;
+					done();
+				});
 		});
 	});
 
 	describe('/GET :director_id', () => {
 		it('Get Director record by ID', (done) => {
-			chai.request(server).get('/api/directors/' + directorId).set('x-access-token', token).end((err, res) => {
-				if (err) {
-					throw err;
-				}
-				res.should.have.status(200);
-				res.body.should.be.a('array'); // Yönetmen kaydı getirirken yönetmene ait filmler de geldiği için array oluyor.
-				done();
-			});
+			chai.request(server)
+				.get('/api/directors/' + directorId)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					if (err)
+						throw err;
+
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					done();
+				});
 		});
 	});
 
@@ -73,28 +83,36 @@ describe('Director tests', () => {
 			const director = {
 				name: 'Mike'
 			};
-			chai.request(server).put('/api/directors/' + directorId).send(director).set('x-access-token', token).end((err, res) => {
-				if (err) {
-					throw err;
-				}
-				res.should.have.status(200);
-				res.body.should.be.a('object');
-				res.body.should.have.property('name').eql(director.name);
-				done();
-			});
+
+			chai.request(server)
+				.put('/api/directors/' + directorId)
+				.send(director)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					if (err)
+						throw err;
+
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('name').eql(director.name);
+					done();
+				});
 		});
 	});
 
 	describe('/DELETE :director_id', () => {
 		it('Delete Director record by id', (done) => {
-			chai.request(server).del('/api/directors/' + directorId).set('x-access-token', token).end((err, res) => {
-				if (err) {
-					throw err;
-				}
-				res.should.have.status(200);
-				res.body.should.be.a('object');
-				done();
-			});
+			chai.request(server)
+				.del('/api/directors/' + directorId)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					if (err)
+						throw err;
+
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					done();
+				});
 		});
 	});
 });
