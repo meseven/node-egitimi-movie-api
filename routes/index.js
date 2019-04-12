6dfac10a8e12a93fe4e0acce5b98636ccdcd6107
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-12 16:35:46 +0300

    test

--- routes/index.js
+++ routes/index.js
@@ -1,89 +1,91 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //Models
-const User = require('../models/User');
+const User = require("../models/User");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+	res.render("index", { title: "Express" });
 });
 
-router.post('/register', async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
 	const { username, password } = req.body;
 
-	try{
-		const userExists = await User.findOne({username});
+	try {
+		const userExists = await User.findOne({ username });
 		if (userExists) {
-		  throw new Error;
+			throw new Error();
 		}
-	}catch (e) {
-		res.json({
+	} catch (e) {
+		res.status(200).json({
 			status: false,
 			errors: {
-				username: 'Username already exists.'
-			}
+				username: "Username already exists.",
+			},
 		});
 		return false;
 	}
 
-
-	bcrypt.hash(password, 10).then((hash) => {
+	bcrypt.hash(password, 10).then(hash => {
 		const user = new User({
 			username,
-			password: hash
+			password: hash,
 		});
 
 		const promise = user.save();
-		promise.then((data) => {
-			res.json(data)
-		}).catch((err) => {
-			res.json(err);
-		})
+		promise
+			.then(({ username, _id }) => {
+				res.json({ username, _id });
+			})
+			.catch(err => {
+				res.json(err);
+			});
 	});
 });
 
-router.post('/authenticate', (req, res) => {
+router.post("/authenticate", (req, res) => {
 	const { username, password } = req.body;
 
-	User.findOne({
-		username
-	}, (err, user) => {
-		if (err)
-			throw err;
+	User.findOne(
+		{
+			username,
+		},
+		(err, user) => {
+			if (err) throw err;
 
-		if(!user){
-			res.json({
-				status: false,
-				message: 'Authentication failed, user not found.'
-			});
-		}else{
-			bcrypt.compare(password, user.password).then((result) => {
-				if (!result){
-					res.json({
-						status: false,
-						message: 'Authentication failed, wrong password.'
-					});
-				}else{
-					const payload = {
-						username
-					};
-					const token = jwt.sign(payload, req.app.get('api_secret_key'), {
-						expiresIn: 720 // 12 saat
-					});
+			if (!user) {
+				res.json({
+					status: false,
+					message: "Authentication failed, user not found.",
+				});
+			} else {
+				bcrypt.compare(password, user.password).then(result => {
+					if (!result) {
+						res.json({
+							status: false,
+							message: "Authentication failed, wrong password.",
+						});
+					} else {
+						const payload = {
+							username,
+						};
+						const token = jwt.sign(payload, req.app.get("api_secret_key"), {
+							expiresIn: 720, // 12 saat
+						});
 
-					res.json({
-						status: true,
-						token
-					})
-				}
-			});
-		}
-	});
-
+						res.json({
+							status: true,
+							token,
+						});
+					}
+				});
+			}
+		},
+	);
 });
 
 module.exports = router;
